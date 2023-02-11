8da793f00ab3249258bfa94322849115b9b77da7
Karan2310 <karandeep9619@gmail.com>
2023-02-12 01:33:38 +0530

    SOS Siren Added

--- client/screens/pages/Sos.js
+++ client/screens/pages/Sos.js
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Audio } from "expo-av"
+import Styles from '../../CommonStyles';
 
 const Sos = () => {
+    const [sound, setSound] = React.useState();
+
+    async function playSound() {
+
+        console.log('Loading Sound');
+        const { sound } = await Audio.Sound.createAsync(require('../../assets/sos.mp3')
+        );
+        setSound(sound);
+        console.log();
+
+        console.log('Playing Sound');
+        await sound.playAsync();
+    }
+
+    React.useEffect(() => {
+        return sound
+            ? () => {
+                console.log('Unloading Sound');
+                sound.unloadAsync();
+            }
+            : undefined;
+    }, [sound]);
+
+
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.sosButton}>
-                <Text style={styles.buttonText}>SOS</Text>
-            </TouchableOpacity>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <View style={{ borderColor: "red", borderWidth: 7, borderRadius: 100, padding: 10 }}>
+                    <TouchableOpacity style={styles.sosButton} onPress={playSound}>
+                        <Text style={styles.buttonText}>SOS</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        // padding: 20,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
     sosButton: {
-        // backgroundColor: 'red',
-        // paddingVertical: 15,
-        // marginVertical:350,
-        // borderRadius: 50
         backgroundColor: "red",
         width: 150,
         height: 150,
         borderRadius: 200,
         alignItems: "center",
         justifyContent: 'center',
-
+        borderWidth: 5,
+        borderColor: 'red',
     },
     buttonText: {
         color: '#fff',
         textAlign: 'center',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        fontSize: 30,
+        fontFamily: Styles.bold.fontFamily,
     }
 })
 
