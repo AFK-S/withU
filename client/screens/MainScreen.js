909a70b9a0f151f0d1cd54f476fb7a631bf3d7b9
SarthakDeshmukh13 <saksard@gmail.com>
2023-02-12 07:54:35 +0530

    Tab bar icons updated

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -1,24 +1,93 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View,Image,Text} from 'react-native'
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 import Sos from './pages/Sos';
 import Map from './pages/Map';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import BottomNav from './BottomNav';
 
 const MainScreen = () => {
     const Tab = createBottomTabNavigator();
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='sos' component={Sos} />
-            <Tab.Screen name='map' component={Map} />
-            <Tab.Screen name='alert' component={Map} />
+        <Tab.Navigator 
+        screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+        position: 'absolute',
+        bottom: 50,
+        left: 20,
+        right: 20,
+        elevation: 0,
+        backgroundColor: '#ffffff',
+        borderRadius: 15,
+        height: 90,
+        ... styles.shadow
+            }
+        }}>
+            <Tab.Screen name='SOS' component={Sos} 
+            options={{
+                tabBarIcon: ({})=> (
+                    <View style={{alignItems:'center',justifyContent:'center', top:10}}>
+                    <Image
+                        source={require('../assets/icons/sos.png')}
+                        resizeMode='contain'
+                        style={{
+                            width:45,
+                            height:45,
+                          
+                        }}/>
+                        <Text style={{fontSize:10}}>SOS</Text>
+                    </View>
+                )
+            }} 
+            />
+            <Tab.Screen name='MAP' component={Map} 
+                        options={{
+                            tabBarIcon: ({})=> (
+                                <View style={{alignItems:'center',justifyContent:'center', top:10}}>
+                                <Image
+                                    source={require('../assets/icons/map.png')}
+                                    resizeMode='contain'
+                                    style={{
+                                        width:45,
+                                        height:45,
+                                      
+                                    }}/>
+                                    <Text style={{fontSize:10}}>MAP</Text>
+                                </View>
+                            )
+                        }} />
+            <Tab.Screen name='ALERT' component={Map}
+                        options={{
+                            tabBarIcon: ({})=> (
+                                <View style={{alignItems:'center',justifyContent:'center', top:10}}>
+                                <Image
+                                    source={require('../assets/icons/alert.png')}
+                                    resizeMode='contain'
+                                    style={{
+                                        width:45,
+                                        height:45,
+                                      
+                                    }}/>
+                                    <Text style={{fontSize:10}}>ALERT</Text>
+                                </View>
+                            )
+                        }}  />
         </Tab.Navigator>
     )
 }
 
 export default MainScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    shadow:{
+        shadowColor:'#FFAACF',
+        shadowOffset:{
+            width:0,
+            height:10
+        },
+        shadowOpacity: 0.35,
+        shadowRadius:4,
+        elevation:5
+
+    }
+})
