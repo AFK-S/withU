d86acc93f23836bb2ba0306326bc697c91f2c8f2
FaizanPotrick <faizan.potrick02@gmail.com>
2023-02-17 02:00:35 +0530

    Remove Extra Parameters

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -69,11 +69,7 @@ const MainScreen = () => {
           bottom: 30,
           left: 30,
           right: 30,
-          bottom: 30,
-          left: 30,
-          right: 30,
           elevation: 0,
-          backgroundColor: "#FFAACF",
           backgroundColor: "#FFAACF",
           borderRadius: 25,
         },
@@ -93,11 +89,6 @@ const MainScreen = () => {
                 padding: 20,
                 borderRadius: 15,
                 aspectRatio: 1,
-                top: Platform.OS === "android" ? 0 : 15,
-                backgroundColor: focused ? "#fff" : "transparent",
-                padding: 20,
-                borderRadius: 15,
-                aspectRatio: 1,
               }}
             >
               <Image
@@ -106,7 +97,6 @@ const MainScreen = () => {
                 style={{
                   width: 35,
                   height: 35,
-                  // tintColor: focused ? "#fff" : "#000",
                   // tintColor: focused ? "#fff" : "#000",
                 }}
               />
