7331dba9edc91236942515d25261a26a7a9feb21
FaizanPotrick <faizan.potrick02@gmail.com>
2023-02-16 17:27:02 +0530

    Add Phone Call Functionality

--- client/screens/pages/Map.js
+++ client/screens/pages/Map.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
-import MapView, { Circle, Marker } from "react-native-maps";
+import call from "react-native-phone-call";
 
 const Map = ({ socket, User, location }) => {
   const [activeUsers, setActiveUsers] = useState([]);
 
-  socket.emit("Get_Active_Users", (users) => {
-    setActiveUsers(users);
-  });
+  useEffect(() => {
+    socket.emit("Get_Active_Users", (users) => {
+      setActiveUsers(users);
+    });
+  }, []);
 
   socket.on("Send_Active_Users", (users) => {
     setActiveUsers(users);
   });
+
+  const triggerCall = (phone_number) => {
+    if (phone_number.length != 10) {
+      return console.error("Invalid Number");
+    }
+    call({
+      number: phone_number,
+      prompt: true,
+    }).catch(console.error);
+  };
 
   return (
     <View
@@ -41,9 +54,59 @@ const Map = ({ socket, User, location }) => {
                 key={index}
                 coordinate={user.coordinates}
                 title={user.user_id}
-                description={user.name}
                 opacity={user.user_id === User.user_id ? 1 : 0.5}
-              />
+              >
+                <Callout
+                  onPress={() => {
+                    if (user.user_id !== User.user_id) {
+                      triggerCall(user.phone_number);
+                    }
+                  }}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Text
+                        style={{
+                          textTransform: "capitalize",
+                          fontSize: 18,
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {user.name}
+                      </Text>
+                      <Text
+                        style={{
+                          textTransform: "capitalize",
+                          fontSize: 12,
+                        }}
+                      >
+                        {user.gender}
+                      </Text>
+                    </View>
+                    {user.user_id !== User.user_id && (
+                      <Text
+                        style={{
+                          fontSize: 20,
+                          marginLeft: 10,
+                        }}
+                      >
+                        📞
+                      </Text>
+                    )}
+                  </View>
+                </Callout>
+              </Marker>
             );
           })}
           <Circle
