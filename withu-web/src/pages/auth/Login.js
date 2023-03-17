df8983814095a5a092db715f787fb9e1b9201f10
SarthakDeshmukh13 <saksard@gmail.com>
2023-03-17 08:50:04 +0530

    Login functionality updated

--- withu-web/src/pages/auth/Login.js
+++ withu-web/src/pages/auth/Login.js
@@ -1,35 +1,120 @@
+import { useToggle, upperFirst } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
-import { TextInput, Button, Box } from "@mantine/core";
-import { PasswordInput } from "@mantine/core";
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+  Container,
+} from "@mantine/core";
+import { NavLink } from "react-router-dom";
 
-const Login = () => {
+export function Login() {
+  const [type, toggle] = useToggle(["login", "register"]);
   const form = useForm({
-    initialValues: { name: "", password: "" },
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      terms: true,
+    },
+
     validate: {
-      name: (value) =>
-        value.length < 2 ? "Name must have at least 2 letters" : null,
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val) =>
+        val.length <= 6
+          ? "Password should include at least 6 characters"
+          : null,
     },
   });
 
   return (
-    <Box maw={320} mx="auto">
-      <form onSubmit={form.onSubmit(console.log)}>
-        <TextInput
-          label="Branch Name"
-          placeholder="Branch Name"
-          {...form.getInputProps("name")}
-        />
-        <PasswordInput
-          label="Password"
-          placeholder="Password"
-          {...form.getInputProps("password")}
-        />
-        <Button type="submit" mt="sm">
-          Submit
-        </Button>
-      </form>
-    </Box>
+    <Container size={500} my={200}>
+      <Paper radius="md" p="xl" withBorder>
+        <Text size="lg" weight={600} align="center">
+          Welcome to withU
+        </Text>
+
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Stack>
+            {type === "register" && (
+              <TextInput
+                label="Name"
+                placeholder="Your name"
+                value={form.values.name}
+                onChange={(event) =>
+                  form.setFieldValue("name", event.currentTarget.value)
+                }
+                radius="md"
+              />
+            )}
+
+            <TextInput
+              required
+              label="Email"
+              placeholder="hello@hello.com"
+              value={form.values.email}
+              onChange={(event) =>
+                form.setFieldValue("email", event.currentTarget.value)
+              }
+              error={form.errors.email && "Invalid email"}
+              radius="md"
+            />
+
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              value={form.values.password}
+              onChange={(event) =>
+                form.setFieldValue("password", event.currentTarget.value)
+              }
+              error={
+                form.errors.password &&
+                "Password should include at least 8 characters"
+              }
+              radius="md"
+            />
+
+            {type === "register" && (
+              <Checkbox
+                label="I accept terms and conditions"
+                checked={form.values.terms}
+                onChange={(event) =>
+                  form.setFieldValue("terms", event.currentTarget.checked)
+                }
+              />
+            )}
+          </Stack>
+
+          <Group position="apart" mt="xl">
+            <Anchor
+              component="button"
+              type="button"
+              color="dimmed"
+              onClick={() => toggle()}
+              size="xs"
+            >
+              {type === "register"
+                ? "Already have an account? Login"
+                : "Don't have an account? Register"}
+            </Anchor>
+            <NavLink to="/dashboard">
+              <Button type="submit" radius="xl" color={"pink"}>
+                {upperFirst(type)}
+              </Button>
+            </NavLink>
+          </Group>
+        </form>
+      </Paper>
+    </Container>
   );
-};
+}
 
 export default Login;
