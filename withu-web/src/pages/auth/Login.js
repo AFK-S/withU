470ec7538e6b3eb5ed987b2153a1bc94f74d7b28
Karan2310 <karandeep9619@gmail.com>
2023-03-17 11:37:52 +0530

    Login UI Updated

--- withu-web/src/pages/auth/Login.js
+++ withu-web/src/pages/auth/Login.js
@@ -31,55 +31,72 @@ export function Login() {
   });
 
   return (
-    <Container size={500} my={200}>
-      <Paper radius="md" p="xl" withBorder>
-        <Text size="lg" weight={600} align="center">
-          Welcome to withU
-        </Text>
+    <div
+      style={{
+        backgroundColor: "#e5e5e5",
+        display: "flex",
+        minHeight: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <Container size={500} my={200}>
+        <Paper radius="md" p="xl" withBorder style={{ widt: "30vw" }}>
+          <Text size="xl" weight={600} align="center" my={15} px={80}>
+            Welcome to withU Admin
+          </Text>
 
-        <form onSubmit={form.onSubmit((val) => console.log(val))}>
-          <Stack>
-            <TextInput
-              required
-              label="User Name"
-              placeholder="Usernmame here"
-              value={form.values.userName}
-              onChange={(event) =>
-                form.setFieldValue("userName", event.currentTarget.value)
-              }
-              error={form.errors.userName && "Invalid userName"}
-              radius="md"
-            />
+          <form onSubmit={form.onSubmit((val) => console.log(val))}>
+            <Stack>
+              <TextInput
+                my={10}
+                required
+                label="Username"
+                placeholder="Username here"
+                value={form.values.userName}
+                onChange={(event) =>
+                  form.setFieldValue("userName", event.currentTarget.value)
+                }
+                error={form.errors.userName && "Invalid userName"}
+                radius="md"
+              />
 
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              value={form.values.password}
-              onChange={(event) =>
-                form.setFieldValue("password", event.currentTarget.value)
-              }
-              error={
-                form.errors.password &&
-                "Password should include at least 8 characters"
-              }
-              radius="md"
-            />
-          </Stack>
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                value={form.values.password}
+                onChange={(event) =>
+                  form.setFieldValue("password", event.currentTarget.value)
+                }
+                error={
+                  form.errors.password &&
+                  "Password should include at least 8 characters"
+                }
+                radius="md"
+              />
+            </Stack>
 
-          <Group position="apart" mt="xl">
-            <NavLink to="/register">
-              <Anchor component="button" type="button" color="dimmed" size="xs">
-                Don't have an account? Register
-              </Anchor>
-            </NavLink>
-            <Button type="submit" radius="xl" color={"pink"}>
-              Login
-            </Button>
-          </Group>
-        </form>
-      </Paper>
-    </Container>
+            <Group position="apart" mt={30}>
+              <Button type="submit" radius="xl" fullWidth color={"pink"}>
+                Login
+              </Button>
+              <NavLink to="/register">
+                <Anchor
+                  component="button"
+                  type="button"
+                  color="dimmed"
+                  size="xs"
+                  align="center"
+                  fullWidth
+                >
+                  Don't have an account? Register
+                </Anchor>
+              </NavLink>
+            </Group>
+          </form>
+        </Paper>
+      </Container>
+    </div>
   );
 }
 
