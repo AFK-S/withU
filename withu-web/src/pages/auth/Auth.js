c90be1c04495c285cb1769c94120abd57f321a5b
SarthakDeshmukh13 <saksard@gmail.com>
2023-03-17 12:01:59 +0530

    Dashboard page updated

--- withu-web/src/pages/auth/Auth.js
+++ withu-web/src/pages/auth/Auth.js
@@ -3,11 +3,13 @@ import Login from "./Login";
 import Register from "./Register";
 import { Routes, Route } from "react-router-dom";
 import Error from "../Error";
+import Dashboard from "../MainScreen/Dashboard";
 const Auth = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
