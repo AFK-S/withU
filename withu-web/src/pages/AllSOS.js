b93737644d93396b7db9d5208bb0b35a66c2edc5
SarthakDeshmukh13 <saksard@gmail.com>
2023-03-17 18:46:41 +0530

    SOS tab updated

--- withu-web/src/pages/AllSOS.js
+++ withu-web/src/pages/AllSOS.js
@@ -1,7 +1,71 @@
 import React from "react";
-
+import {
+  Card,
+  Avatar,
+  Text,
+  Progress,
+  Badge,
+  Group,
+  ActionIcon,
+  Grid,
+} from "@mantine/core";
+import { IconShield } from "@tabler/icons-react";
 const AllSOS = () => {
-  return <div>AllSOS</div>;
+  const data = [
+    {
+      name: "Name1",
+      phone: "1234567890",
+      location: "Location1",
+      time: "11:00:23 AM",
+    },
+    {
+      name: "Name2",
+      phone: "1234567890",
+      location: "Location1",
+      time: "11:00:23 AM",
+    },
+    {
+      name: "Name3",
+      phone: "1234567890",
+      location: "Location1",
+      time: "11:00:23 AM",
+    },
+  ];
+  return (
+    <div>
+      <Grid gutterXl={30}>
+        {data.map((item) => {
+          const { name, phone, location, time } = item;
+
+          return (
+            <Grid.Col span={4}>
+              <Card withBorder padding="lg" radius="md">
+                <Group position="apart">
+                  <div className="avatar">
+                    <IconShield />
+                  </div>
+                  <Badge color="pink" p={5}>
+                    {time}
+                  </Badge>
+                </Group>
+
+                <Text fz="lg" fw={500} mt="lg">
+                  {name}
+                </Text>
+                <Text fz="sm" c="dimmed" mt={5}>
+                  {phone}
+                </Text>
+
+                <Text c="dimmed" fz="sm" mt="md">
+                  Location: {location}
+                </Text>
+              </Card>
+            </Grid.Col>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 };
 
 export default AllSOS;
