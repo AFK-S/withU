d277825237f6c3f4024a7135b11ec7cc89f6921d
Karan2310 <karandeep9619@gmail.com>
2023-03-17 08:29:31 +0530

    Error Page Added

--- withu-web/src/pages/auth/Register.js
+++ withu-web/src/pages/auth/Register.js
@@ -11,45 +11,69 @@ import {
   Group,
   Button,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
+  const form = useForm({
+    initialValues: { branch: "", password: "" },
+    validate: {
+      branch: (value) => (value.length < 3 ? "Enter valid branch" : null),
+      password: (value) =>
+        value.length < 8 ? "Enter valid password (min length : 8)" : null,
+    },
+  });
   return (
-    <Container size={420} my={40}>
-      <Title
-        align="center"
-        sx={(theme) => ({
-          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-          fontWeight: 900,
-        })}
-      >
-        Welcome back!
-      </Title>
-      <Text color="dimmed" size="sm" align="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" component="button">
-          Create account
-        </Anchor>
-      </Text>
+    <div
+      className="conatiner"
+      style={{
+        backgroundColor: "#e5e5e5",
+        display: "flex",
+        minHeight: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <Container size={520} my={40}>
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            fontWeight: 800,
+          })}
+        >
+          Register A Police Station
+        </Title>
 
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group position="apart" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
-      </Paper>
-    </Container>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <form onSubmit={form.onSubmit(console.log)}>
+            <TextInput
+              label="Branch Name"
+              placeholder="Branch Name"
+              {...form.getInputProps("branch")}
+              radius="md"
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Password"
+              {...form.getInputProps("password")}
+              radius="md"
+              mt={15}
+            />
+            <Button type="submit" fullWidth mt="xl" radius="md" color={"pink"}>
+              Register
+            </Button>
+          </form>
+          <Text color="dimmed" size="sm" align="center" mt={15}>
+            Back to {``}
+            <NavLink to="/">
+              <Anchor size="sm" component="button">
+                Login
+              </Anchor>
+            </NavLink>
+          </Text>
+        </Paper>
+      </Container>
+    </div>
   );
 };
 
