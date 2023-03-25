0b22df36cc986f078ea0d5072b85561d04d2d69f
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-25 13:42:44 +0530

    Update Backend API

--- server/controllers/Credentials.js
+++ server/controllers/Credentials.js
@@ -40,9 +40,7 @@ const Register = async (req, res) => {
 const Login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(400)
-      .json({ type: "error", message: errors.array()[0].msg });
+    return res.status(400).send(errors.array()[0].msg);
   }
   const { email_address, password } = req.body;
   try {
