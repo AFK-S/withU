60490b0c16796f18cd8e978bafb861e7fb262f3a
adi50920 <adityarai50920@gmail.com>
2023-03-25 15:32:16 +0530

    Icons updated

--- client/screens/pages/Map.js
+++ client/screens/pages/Map.js
@@ -1,43 +1,43 @@
-import MapView, { Circle, Marker } from "react-native-maps";
-import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import MapView, { Circle, Marker } from 'react-native-maps'
+import React, { useState, useEffect } from 'react'
+import { View, Text, Image } from 'react-native'
 
 const Map = ({ socket, User, location }) => {
-  const [activeUsers, setActiveUsers] = useState([]);
-  const [Police, setPolice] = useState([]);
-  const [SOS, setSOS] = useState([]);
+  const [activeUsers, setActiveUsers] = useState([])
+  const [Police, setPolice] = useState([])
+  const [SOS, setSOS] = useState([])
 
   useEffect(() => {
     if (socket.connected) {
-      socket.emit("Get_Meter_Active_Users", (users) => {
-        setActiveUsers(users);
-      });
-      socket.emit("Get_Police", (police) => {
-        setPolice(police);
-      });
-      socket.emit("Get_SOS", (sos) => {
-        setSOS(sos);
-      });
+      socket.emit('Get_Meter_Active_Users', (users) => {
+        setActiveUsers(users)
+      })
+      socket.emit('Get_Police', (police) => {
+        setPolice(police)
+      })
+      socket.emit('Get_SOS', (sos) => {
+        setSOS(sos)
+      })
     }
-  }, [location]);
+  }, [location])
 
-  socket.on("Send_Active_Users", (users) => {
-    setActiveUsers(users);
-  });
+  socket.on('Send_Active_Users', (users) => {
+    setActiveUsers(users)
+  })
 
   return (
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       {location !== null ? (
         <MapView
           style={{
-            width: "100%",
-            height: "100%",
+            width: '100%',
+            height: '100%',
           }}
           region={{
             latitude: location.latitude,
@@ -54,7 +54,19 @@ const Map = ({ socket, User, location }) => {
                 coordinate={user.coordinates}
                 opacity={user.user_id === User.user_id ? 1 : 0.6}
               />
-            );
+
+              // <Marker
+              //   key={index}
+              //   coordinate={user.coordinates}
+              //   opacity={user.user_id === User.user_id ? 1 : 0.6}
+              // >
+              //   <Image
+              //     source={require('../../assets/placeholder.png')}
+              //     style={{ width: 40, height: 40 }}
+              //     resizeMode="contain"
+              //   />
+              // </Marker>
+            )
           })}
           {Police.map((police, index) => {
             return (
@@ -62,33 +74,38 @@ const Map = ({ socket, User, location }) => {
                 key={index}
                 title={police.branch_name}
                 coordinate={police.coordinates}
-                pinColor={"black"}
-              />
-            );
+              >
+                <Image
+                  source={require('../../assets/police.png')}
+                  style={{ width: 40, height: 40 }}
+                  resizeMode="contain"
+                />
+              </Marker>
+            )
           })}
           {SOS.map((sos, index) => {
             return (
               <Circle
                 center={sos.coordinates}
                 radius={30}
-                fillColor={"rgba(255,0,0,0.05)"}
-                strokeColor={"rgba(255,0,0,0.0)"}
+                fillColor={'rgba(255,0,0,0.05)'}
+                strokeColor={'rgba(255,0,0,0.0)'}
                 strokeWidth={0}
                 key={index}
               />
-            );
+            )
           })}
           <Circle
             center={location}
             radius={3000}
-            fillColor={"rgba(0,0,0,0.1)"}
+            fillColor={'rgba(0,0,0,0.1)'}
           />
         </MapView>
       ) : (
         <Text>Waiting for location</Text>
       )}
     </View>
-  );
-};
+  )
+}
 
-export default Map;
+export default Map
