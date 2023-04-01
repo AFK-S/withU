6d785e1b245465f97bd58931541d1e8fdd17dae9
adi50920 <adityarai50920@gmail.com>
2023-04-02 02:57:47 +0530

    Update Chart Room

--- server/models/Police.js
+++ server/models/Police.js
@@ -1,15 +1,16 @@
-const { Schema, connection } = require("mongoose");
+const { Schema, connection } = require('mongoose')
 
 const Police = new Schema(
   {
     name: {
       type: String,
       trim: true,
+      unique: true,
       match: [
         /^[a-zA-Z0-9]+$/,
         (props) => `${props.value} is not a valid username`,
       ],
-      required: [true, "Please add a Username"],
+      required: [true, 'Please add a Username'],
     },
     branch_name: {
       type: String,
@@ -18,30 +19,30 @@ const Police = new Schema(
         /^[a-zA-Z0-9]+$/,
         (props) => `${props.value} is not a valid branch name`,
       ],
-      required: [true, "Please add a Branch Name"],
+      required: [true, 'Please add a Branch Name'],
     },
     type_of_user: {
       type: String,
       trim: true,
-      enum: ["police", "hospital"],
+      enum: ['police', 'hospital'],
       match: [
         /^(police|hospital)$/,
         (props) => `${props.value} is not a valid type of user`,
       ],
-      required: [true, "Please add a Type of User"],
+      required: [true, 'Please add a Type of User'],
     },
     coordinates: {
       type: Object,
-      required: [true, "Please add a Coordinates"],
+      required: [true, 'Please add a Coordinates'],
     },
     password: {
       type: String,
-      required: [true, "Please add a Password"],
+      required: [true, 'Please add a Password'],
     },
   },
   {
     timestamps: true,
-  }
-);
+  },
+)
 
-module.exports = connection.useDb("WithU").model("Police", Police);
+module.exports = connection.useDb('WithU').model('Police', Police)
